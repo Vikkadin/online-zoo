c85e7c45144a99b184e075880a8f935a83ce1718
Vikkadin <104149770+Vikkadin@users.noreply.github.com>
2022-12-06 00:15:59 +0200

    feat: add burger js

--- main.js
+++ main.js
@@ -18,3 +18,45 @@ document.addEventListener('DOMContentLoaded', function () {
     navigation.addEventListener('mousedown', makeActive);
 
 });
+
+
+
+
+
+const burger = document.querySelector('.burger');
+const navMenuMobile = document.querySelector('.logo_and_menu');
+const logoMobile = document.querySelector('.logo-burger');
+const pageFader = document.querySelector('.page-fade');
+
+
+function navSwitcher(el) {
+
+    let target = el.target;
+
+    if (target === burger) {
+
+        burger.classList.toggle('burger-x')
+        navMenuMobile.classList.toggle('burger-open');
+        logoMobile.classList.toggle('logo-fader');
+        pageFader.classList.toggle('displayed');
+        pageFader.classList.toggle('page-fader');
+
+    } else if ((target.closest('.nav__item') || target.closest('.designed')) && !target.closest('.current-page')) {
+
+        burger.classList.remove('burger-x')
+        navMenuMobile.classList.remove('burger-open');
+        logoMobile.classList.remove('logo-fader');
+        pageFader.classList.remove('displayed', 'page-fader');
+
+    } else if (target != navMenuMobile && !target.closest('.nav-block') && !target.closest('.nav-wrapper')) {
+
+        burger.classList.remove('burger-x')
+        navMenuMobile.classList.remove('burger-open');
+        logoMobile.classList.remove('logo-fader');
+        pageFader.classList.remove('displayed', 'page-fader');
+
+    }
+
+}
+
+document.addEventListener('click', navSwitcher);
