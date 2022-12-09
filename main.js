7f3c9db193bc6969ead3653362c8488d052db19c
Vikkadin <104149770+Vikkadin@users.noreply.github.com>
2022-12-09 19:22:56 +0200

    feat: implement animals cards slider

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+/* current page */
+
 document.addEventListener('DOMContentLoaded', function () {
     const selector = '.nav__link';
     const elems = Array.from(document.querySelectorAll(selector));
@@ -19,9 +21,7 @@ document.addEventListener('DOMContentLoaded', function () {
 
 });
 
-
-
-
+/* burger menu */
 
 const burger = document.querySelector('.burger');
 const navMenuMobile = document.querySelector('.logo_and_menu');
@@ -60,3 +60,70 @@ function navSwitcher(el) {
 }
 
 document.addEventListener('click', navSwitcher);
+
+
+/* animals slider */
+
+const animalCardsBlock = document.querySelector('.cards-block');
+const sliderLeft = document.querySelector('.slider-left');
+const sliderCenter = document.querySelector('.slider-center');
+const sliderRight = document.querySelector('.slider-right');
+const slideLeftBtn = document.querySelector('.animals-block__left-btn');
+const slideRightBtn = document.querySelector('.animals-block__right-btn');
+let animalCards = Array.from(sliderCenter.getElementsByClassName('animal-card'));
+
+
+function showRandom(arr) {
+    for (let i = arr.length - 1; i > 0; i--) {
+        let j = Math.floor(Math.random() * (i + 1));
+
+        [arr[i], arr[j]] = [arr[j], arr[i]];
+    }
+}
+
+
+slideLeftBtn.addEventListener('click', () => {
+
+    slideLeftBtn.setAttribute('disabled', 'disabled');
+    slideRightBtn.setAttribute('disabled', 'disabled');
+    let centerClone = sliderCenter.cloneNode(true);
+    showRandom(animalCards);
+
+    for (let item of animalCards) {
+        sliderLeft.append(item);
+    }
+
+    let leftClone = sliderLeft.cloneNode(true);
+    sliderCenter.innerHTML = centerClone.innerHTML;
+    animalCardsBlock.classList.add('cards-block-left', 'cards-block-non-click');
+
+    animalCardsBlock.addEventListener('animationend', () => {
+        animalCardsBlock.classList.remove('cards-block-left', 'cards-block-non-click');
+        sliderCenter.innerHTML = leftClone.innerHTML;
+        slideLeftBtn.removeAttribute('disabled');
+        slideRightBtn.removeAttribute('disabled');
+    })
+});
+
+slideRightBtn.addEventListener('click', () => {
+
+    slideLeftBtn.setAttribute('disabled', 'disabled');
+    slideRightBtn.setAttribute('disabled', 'disabled');
+    let centerClone = sliderCenter.cloneNode(true);
+    showRandom(animalCards);
+
+    for (let item of animalCards) {
+        sliderRight.append(item);
+    }
+
+    let rightClone = sliderRight.cloneNode(true);
+    sliderCenter.innerHTML = centerClone.innerHTML;
+    animalCardsBlock.classList.add('cards-block-right', 'cards-block-non-click');
+
+    animalCardsBlock.addEventListener('animationend', () => {
+        animalCardsBlock.classList.remove('cards-block-right', 'cards-block-non-click');
+        sliderCenter.innerHTML = rightClone.innerHTML;
+        slideLeftBtn.removeAttribute('disabled');
+        slideRightBtn.removeAttribute('disabled');
+    })
+});
